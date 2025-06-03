44fb5cfa0961f188a1513cf68902d29a63071e6d
Tony Blum <tonyblum@me.com>
2025-06-02 18:23:03 -0600

    🚀 wolodev: stable Cosmos chain, REST/gRPC working

--- app/app.go
+++ app/app.go
@@ -110,9 +110,6 @@ import (
 	ibctm "github.com/cosmos/ibc-go/v7/modules/light-clients/07-tendermint"
 	"github.com/spf13/cast"
 
-	wolodevmodule "wolodev/x/wolodev"
-	wolodevmodulekeeper "wolodev/x/wolodev/keeper"
-	wolodevmoduletypes "wolodev/x/wolodev/types"
 	// this line is used by starport scaffolding # stargate/app/moduleImport
 
 	appparams "wolodev/app/params"
@@ -121,7 +118,7 @@ import (
 
 const (
 	AccountAddressPrefix = "cosmos"
-	Name                 = "wolodev"
+	Name                 = "freshgo"
 )
 
 // this line is used by starport scaffolding # stargate/wasm/app/enabledProposals
@@ -173,7 +170,6 @@ var (
 		ica.AppModuleBasic{},
 		vesting.AppModuleBasic{},
 		consensus.AppModuleBasic{},
-		wolodevmodule.AppModuleBasic{},
 		// this line is used by starport scaffolding # stargate/app/moduleBasic
 	)
 
@@ -249,7 +245,6 @@ type App struct {
 	ScopedTransferKeeper capabilitykeeper.ScopedKeeper
 	ScopedICAHostKeeper  capabilitykeeper.ScopedKeeper
 
-	WolodevKeeper wolodevmodulekeeper.Keeper
 	// this line is used by starport scaffolding # stargate/app/keeperDeclaration
 
 	// mm is the module manager
@@ -296,7 +291,6 @@ func New(
 		govtypes.StoreKey, paramstypes.StoreKey, ibcexported.StoreKey, upgradetypes.StoreKey,
 		feegrant.StoreKey, evidencetypes.StoreKey, ibctransfertypes.StoreKey, icahosttypes.StoreKey,
 		capabilitytypes.StoreKey, group.StoreKey, icacontrollertypes.StoreKey, consensusparamtypes.StoreKey,
-		wolodevmoduletypes.StoreKey,
 		// this line is used by starport scaffolding # stargate/app/storeKey
 	)
 	tkeys := sdk.NewTransientStoreKeys(paramstypes.TStoreKey)
@@ -519,14 +513,6 @@ func New(
 		),
 	)
 
-	app.WolodevKeeper = *wolodevmodulekeeper.NewKeeper(
-		appCodec,
-		keys[wolodevmoduletypes.StoreKey],
-		keys[wolodevmoduletypes.MemStoreKey],
-		app.GetSubspace(wolodevmoduletypes.ModuleName),
-	)
-	wolodevModule := wolodevmodule.NewAppModule(appCodec, app.WolodevKeeper, app.AccountKeeper, app.BankKeeper)
-
 	// this line is used by starport scaffolding # stargate/app/keeperDefinition
 
 	/**** IBC Routing ****/
@@ -588,7 +574,6 @@ func New(
 		params.NewAppModule(app.ParamsKeeper),
 		transferModule,
 		icaModule,
-		wolodevModule,
 		// this line is used by starport scaffolding # stargate/app/appModule
 
 		crisis.NewAppModule(app.CrisisKeeper, skipGenesisInvariants, app.GetSubspace(crisistypes.ModuleName)), // always be last to make sure that it checks for all invariants and not only part of them
@@ -621,7 +606,6 @@ func New(
 		paramstypes.ModuleName,
 		vestingtypes.ModuleName,
 		consensusparamtypes.ModuleName,
-		wolodevmoduletypes.ModuleName,
 		// this line is used by starport scaffolding # stargate/app/beginBlockers
 	)
 
@@ -647,7 +631,6 @@ func New(
 		upgradetypes.ModuleName,
 		vestingtypes.ModuleName,
 		consensusparamtypes.ModuleName,
-		wolodevmoduletypes.ModuleName,
 		// this line is used by starport scaffolding # stargate/app/endBlockers
 	)
 
@@ -678,7 +661,6 @@ func New(
 		upgradetypes.ModuleName,
 		vestingtypes.ModuleName,
 		consensusparamtypes.ModuleName,
-		wolodevmoduletypes.ModuleName,
 		// this line is used by starport scaffolding # stargate/app/initGenesis
 	}
 	app.mm.SetOrderInitGenesis(genesisModuleOrder...)
@@ -903,7 +885,6 @@ func initParamsKeeper(appCodec codec.BinaryCodec, legacyAmino *codec.LegacyAmino
 	paramsKeeper.Subspace(ibcexported.ModuleName)
 	paramsKeeper.Subspace(icacontrollertypes.SubModuleName)
 	paramsKeeper.Subspace(icahosttypes.SubModuleName)
-	paramsKeeper.Subspace(wolodevmoduletypes.ModuleName)
 	// this line is used by starport scaffolding # stargate/app/paramSubspace
 
 	return paramsKeeper
--- cmd/wolodevd/cmd/root.go
+++ cmd/wolodevd/cmd/root.go
@@ -57,7 +57,7 @@ func NewRootCmd() (*cobra.Command, appparams.EncodingConfig) {
 
 	rootCmd := &cobra.Command{
 		Use:   app.Name + "d",
-		Short: "Start wolodev node",
+		Short: "Start freshgo node",
 		PersistentPreRunE: func(cmd *cobra.Command, _ []string) error {
 			// set the default command outputs
 			cmd.SetOut(cmd.OutOrStdout())
